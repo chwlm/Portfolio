21d61a30f2ffec1e85c3c2c6c10c62518f8ac1a0
chwlm <107909884+chwlm@users.noreply.github.com>
2023-07-07 01:25:02 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ const pages = document.querySelectorAll(".page");
 const header_element = document.querySelectorAll(".element-header");
 const education_cards = document.querySelectorAll(".education-card");
 const education_cards_title = document.querySelectorAll(".education-card-title");
+const dots = document.querySelectorAll(".dot");
 let state_page = 0;
 
 change_page = (index) => {
   pages.forEach(
     pages => (pages.style.transform = `translateX(${-(index*100)}%)`)
   )
+  dots.forEach(
+    (dots,i) => ((index===i)? dots.style.background = '#2B2730': dots.style.background = 'white')
+  )
   state_page = index;
   header_element.forEach(
     (head,i) => ((index===i)? head.style.textDecoration = 'underline' :  head.style.textDecoration = 'none')
   )
+  
 }
 
 chage_education_card = (index) => {
